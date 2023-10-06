8258bf45182fab74e1b2f8c4a33151b2cdcafa2b
airlegend111 <128436131+airlegend111@users.noreply.github.com>
2023-10-06 08:02:48 -0500

    Update Hoteles.java

--- Hoteles.java
+++ Hoteles.java
@@ -1,26 +1,51 @@
+import javax.swing.JOptionPane;
+
 /**
  * Hoteles
  */
-public class Hoteles {
-public static int [] altura(int [] pisos){
-    pisos[0] = (int)((Math.random()* 15)+3);
-    
-    return pisos;
-}
-public static int [] cadena(int [] hoteles){
-    hoteles[0] = (int)((Math.random()* 7)+1);
-    return hoteles;
+public class Hoteles { // declaramos una clase publica llamada hoteles
 
-}
-public static int [] time(int []horas){
-    horas[0] = (int)((Math.random()* 24)+0);
-    return horas;
-    
-}
-public static int[] luz(int [] tipo){
-    tipo[0]= (int)((Math.random()*3)+1);
-    return tipo;
+    public static void main(String[] args) {
+        int cantidadHoteles = (int) ((Math.random() * 7) + 1); // la variable cantidadHoteles genera aleatoriamentem la
+                                                               // cantidad de hoteles del 1 al 7
 
-}
+        double hoteles[][] = new double[cantidadHoteles][4]; // La matriz hoteles en la primera posicionde va tomar el
+                                                             // valor generado por cantidadHoteles para crear n listas
+                                                             // de listas
+                                                             // En el segundo parametro de la matriz dicta que cada
+                                                             // lista va a tener 4 slots de espacios para alojar la
+                                                             // informacion
+        for (int x = 0; x < cantidadHoteles; x++) { // el for se repite n cantidad de veces dependiendo la
+                                                    // cantiadadHoteles y se va a repetir mientras x sea menor a
+                                                    // cantiadadHoteles
+            hoteles[x][0] = (int) ((Math.random() * 14) + 3); // en hoteles[x][0] se genera aleotariamente la cantidad
+                                                              // de pisos
+            hoteles[x][1] = (int) ((Math.random() * 3) + 1); // en hoteles[x][1] se genera aleatoriamente el tipo de
+                                                             // luces
+            hoteles[x][2] = (int) ((Math.random() * 24) + 0);// en hoteles[x][2] se genera aleatoriamente las horas de
+                                                             // uso
 
+            if (hoteles[x][1] == 1) { // el if dice si el tipo de luces es == n va a multiplicar por el respectivo
+                                      // precio el total de horas en las que todas las luces estaban encendidad entre
+                                      // todos los pisos
+                hoteles[x][3] = hoteles[x][2] * 0.50;
+
+            }
+            if (hoteles[x][1] == 2) {
+                hoteles[x][3] = hoteles[x][2] * 0.25;
+
+            }
+            if (hoteles[x][1] == 3) {
+                hoteles[x][3] = hoteles[x][2] * 1;
+            }
+        }
+
+        for (int y = 0; y < cantidadHoteles; y++) { // Este ultimo siclo se encarga de imprimir todos los datos
+                                                    // requeridos
+            JOptionPane.showMessageDialog(null, "para el hotel # " + (y + 1) + " hubo luces por " + hoteles[y][2]
+                    + " y el gasto fue $ " + hoteles[y][3]);
+
+        }
+
+    }
 }
