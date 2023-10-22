8a503f4503df35fb9097f414edd215456ebb4ca0
airlegend111 <128436131+airlegend111@users.noreply.github.com>
2023-10-21 23:42:32 -0500

    Update taller4.java

--- taller4.java
+++ taller4.java
@@ -4,35 +4,26 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Map.Entry;
 
-/**
- * taller4
- */
 public class taller4 {
 
     static Random random = new Random();
 
-    static String[] getNombresDeCasas(int size) {
+    static String getNombreDeCasa() {
         String[] prefijos = { "la", "una" };
         String[] mid = { "cueva", "casa", "mansión" };
         String[] suf = {
-                "de la montaña",
-                "del bosque",
-                "del pantano",
-                "del desierto",
-                "de Drácula",
-                "de la bruja",
-                "del vampiro",
+            "de la montaña", "del bosque", "del pantano",
+            "del desierto", "de Drácula", "de la bruja", "del vampiro",
         };
 
         return prefijos[random.nextInt(prefijos.length)] + " " 
-        + mid[random.nextInt(mid.length)] + " " 
-        + suf[random.nextInt(suf.length)];
+             + mid[random.nextInt(mid.length)] + " " 
+             + suf[random.nextInt(suf.length)];
     }
 
-    static int getPrecios() {
+    static int getPrice() {
         return random.nextInt(1000000);
     }
-
 
     public static void main(String[] args) {
         String[] nombreBarrios = { "Floresta", "Aranjuez", "Manrique", "Carlos E", "Robledo" };
@@ -56,12 +47,12 @@ public class taller4 {
         do {
             String casa = getNombreDeCasa();
             String barrio = nombreBarrios[random.nextInt(nombreBarrios.length)];
-            int precio = getPrecios() * pesos.get(barrio);
+            int precio = getPrice() * pesos.get(barrio);
 
             System.out.println("------*-------");
             System.out.println(casa);
             System.out.println(barrio);
-            System.out.println(precio + " pesos");
+            System.out.println(precio + " Bolívares");
             System.out.println("------*-------");
 
             try {
@@ -81,7 +72,37 @@ public class taller4 {
         System.out.println("Reporte de ventas:");
 
         for (String barrio : ventas.keySet()) {
-            System.out.println(barrio + " vendió " + ventas.get(barrio) + " pesos y " + cantidadVentas.get(barrio) + " casas.");
+            System.out.println(barrio + " vendió " + ventas.get(barrio) + " Bolívares y " + cantidadVentas.get(barrio) + " casas.");
         }
+
+        // Reportes finales.
+        System.out.println("\nResumen:");
+
+        Entry<String, Integer> maxEntry = null;
+        for (Entry<String, Integer> entry : ventas.entrySet()) {
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0 
+                || (entry.getValue().compareTo(maxEntry.getValue()) == 0 && pesos.get(entry.getKey()) < pesos.get(maxEntry.getKey()))) {
+                maxEntry = entry;
+            }
+        }
+        System.out.println("Barrio que más dinero ganó: " + maxEntry.getKey());
+
+        Entry<String, Integer> minEntry = null;
+        for (Entry<String, Integer> entry : ventas.entrySet()) {
+            if (minEntry == null || entry.getValue().compareTo(minEntry.getValue()) < 0
+                || (entry.getValue().compareTo(minEntry.getValue()) == 0 && pesos.get(entry.getKey()) > pesos.get(minEntry.getKey()))) {
+                minEntry = entry;
+            }
+        }
+        System.out.println("Barrio que menos dinero ganó: " + minEntry.getKey());
+
+        maxEntry = null;
+        for (Entry<String, Integer> entry : cantidadVentas.entrySet()) {
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0 
+                || (entry.getValue().compareTo(maxEntry.getValue()) == 0 && pesos.get(entry.getKey()) > pesos.get(maxEntry.getKey()))) {
+                maxEntry = entry;
+            }
+        }
+        System.out.println("Barrio que más casas vendió: " + maxEntry.getKey());
     }
 }
