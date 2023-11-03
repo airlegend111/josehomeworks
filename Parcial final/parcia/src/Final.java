bb379b3f791586a253ced95339c4dc4186569b35
airlegend111 <128436131+airlegend111@users.noreply.github.com>
2023-11-03 07:24:59 -0500

    commit2

--- Parcial final/parcia/src/Final.java
+++ Parcial final/parcia/src/Final.java
@@ -6,42 +6,40 @@ import java.util.Map.Entry;
 import java.io.*;
 
 public class Final {
-    public static int Doc
-   
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        File archivo = null;
-        FileReader fr = null;
-        BufferedReader br = null;
-        Stringc[] diaSemana = {"lunes", "martes"}
-        for (int i = 0; i < 7; i ++) {
-           archivo = new File("C:\\Users\\ASUS\\Desktop\\Parcial final\\parcia\\lunes.txt"+"C:\\Users\\ASUS\\Desktop\\Parcial final\\parcia\\martes.txt"+"C:\\Users\\ASUS\\Desktop\\Parcial final\\parcia\\miercoles.txt" + ""); 
-        try {
-            archivo = new File("prueba.txt");
-            fr = new FileReader(archivo);
 
-            br = new BufferedReader(fr);
+        String[] diaSemana = { "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo" };
+        String[][] lunesdata, martesdata, miercolesdata, juevesdata, viernesdata, sabadodata,
+                domingodata = new String[100][5];
 
-            String linea;
-            while ((linea = br.readLine()) != null){
-               System.out.println(linea);
-            }
-               
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Error al leer el archivo");
-        } finally {
+        for (int i = 0; i < 100; i++) {
+            FileReader fr = null;
+            BufferedReader br = null;
             try {
-                if (null != fr) {
-                    fr.close();
+                fr = new FileReader(diaSemana[i] + ".txt");
+                String linea = "id;monto;emisor;receptor;hora";
+                linea.split(";");
+                while ((linea = br.readLine()) != null) {
+                    System.out.println(linea);
                 }
-            } catch (Exception e2) {
-                System.out.println(e2.getMessage());;
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Error al leer el archivo");
+            } finally {
+                try {
+                    if (null != fr) {
+                        fr.close();
+                    }
+                } catch (Exception e2) {
+                    System.out.println(e2.getMessage());
+                    ;
+                }
             }
         }
-    }
-        
-    }
         Scanner.close();
     }
 
+}
